628bf0afae116056184c87cfb82b11dcd7eca17b
Sherali <sherali.uz@gmail.com>
2022-06-17 14:04:11 +0500

    fix: typeorm synchronize = fals and logging false for live

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,15 @@
-import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+import { config } from "dotenv";
 
-export const ONE_TEST_PRICE = 20000 // One test price
-export const DATE_FORMAT = "DD.MM.YYYY"
-export const DATE_TIME_FORMAT = "DD.MM.YYYY HH:mm:ss"
-export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const ONE_TEST_PRICE = 20000; // One test price
+export const DATE_FORMAT = "DD.MM.YYYY";
+export const DATE_TIME_FORMAT = "DD.MM.YYYY HH:mm:ss";
+export const CREDENTIALS = process.env.CREDENTIALS === "true";
+export const IS_LIVE = process.env.NODE_ENV === "production";
+export const {
+  NODE_ENV,
+  PORT, DB_HOST, DB_PORT,
+  DB_USER, DB_PASSWORD, DB_DATABASE,
+  SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN
+} = process.env;
--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,7 +1,7 @@
 import { join } from "path";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 import { ConnectionOptions } from "typeorm";
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from "@config";
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE, IS_LIVE } from "@config";
 
 export const dbConnection: ConnectionOptions = {
   type: "postgres",
@@ -10,8 +10,8 @@ export const dbConnection: ConnectionOptions = {
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
-  synchronize: true,
-  logging: true,
+  synchronize: false,
+  logging: !IS_LIVE,
   entities: [join(__dirname, "../**/*.entity{.ts,.js}")],
   migrations: [join(__dirname, "../**/*.migration{.ts,.js}")],
   subscribers: [join(__dirname, "../**/*.subscriber{.ts,.js}")],
