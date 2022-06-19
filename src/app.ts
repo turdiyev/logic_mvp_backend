04fd37754d74430665d1a58c4f0bde73ab671861
Sherali <sherali.uz@gmail.com>
2022-06-19 21:43:18 +0500

    chore: sentry error handler

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-import 'reflect-metadata';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import "reflect-metadata";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
-import helmet from 'helmet';
-import hpp from 'hpp';
-import morgan from 'morgan';
-import compression from 'compression';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import { createConnection } from 'typeorm';
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
-import { dbConnection } from '@databases';
-import { Routes } from '@interfaces/routes.interface';
-import errorMiddleware from '@middlewares/error.middleware';
-import { logger, stream } from '@utils/logger';
+import helmet from "helmet";
+import hpp from "hpp";
+import morgan from "morgan";
+import compression from "compression";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { createConnection } from "typeorm";
+import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from "@config";
+import { dbConnection } from "@databases";
+import { Routes } from "@interfaces/routes.interface";
+import errorMiddleware from "@middlewares/error.middleware";
+import { logger, stream } from "@utils/logger";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 class App {
   public app: express.Application;
@@ -22,13 +24,28 @@ class App {
 
   constructor(routes: Routes[]) {
     this.app = express();
-    this.env = NODE_ENV || 'development';
+    this.env = NODE_ENV || "development";
     this.port = PORT || 3000;
 
-    this.env !== 'test' && this.connectToDatabase();
+    this.env !== "test" && this.connectToDatabase();
+
+    Sentry.init({
+      dsn: "https://6de791c7c6454be78e00e946402027cb@o140268.ingest.sentry.io/6515970",
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app: this.app })
+      ],
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0
+    });
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
-    this.initializeSwagger();
+    // this.initializeSwagger();
     this.initializeErrorHandling();
   }
 
@@ -38,8 +55,8 @@ class App {
       logger.info(`======= ENV: ${this.env} =======`);
       logger.info(`🚀 App listening on the port ${this.port}`);
       logger.info(`=================================`);
-    }).on('error', e=>{
-      console.error("App..listner() error -", e)
+    }).on("error", e => {
+      console.error("App..listner() error -", e);
     });
   }
 
@@ -52,11 +69,17 @@ class App {
   }
 
   private initializeMiddlewares() {
+    // RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+    this.app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+    this.app.use(Sentry.Handlers.tracingHandler());
+
     this.app.use(morgan(LOG_FORMAT, { stream }));
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
-    this.app.use(express.static('public'));
-    this.app.use(express.static('uploads'));
+    this.app.use(express.static("public"));
+    this.app.use(express.static("uploads"));
     this.app.use(helmet());
     this.app.use(compression());
     this.app.use(express.json());
@@ -66,7 +89,7 @@ class App {
 
   private initializeRoutes(routes: Routes[]) {
     routes.forEach(route => {
-      this.app.use('/', route.router);
+      this.app.use("/", route.router);
     });
   }
 
@@ -74,19 +97,21 @@ class App {
     const options = {
       swaggerDefinition: {
         info: {
-          title: 'REST API',
-          version: '1.0.0',
-          description: 'Example docs',
-        },
+          title: "REST API",
+          version: "1.0.0",
+          description: "Example docs"
+        }
       },
-      apis: ['swagger.yaml'],
+      apis: ["swagger.yaml"]
     };
 
     const specs = swaggerJSDoc(options);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
   }
 
   private initializeErrorHandling() {
+    // The error handler must be before any other error middleware and after all controllers
+    this.app.use(Sentry.Handlers.errorHandler());
     this.app.use(errorMiddleware);
   }
 }
